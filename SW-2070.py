6b129e05229579e45619ef4673d64a31b4284cfa
ByungYeon Jang <64725424+kisagge@users.noreply.github.com>
2021-01-11 15:31:26 +0900

    Delete SW-2070.py

--- SW-2070.py
+++
@@ -1,9 +0,0 @@
-TC = int(input())
-for tc in range(1, TC+1):
-    n1, n2 = map(int, input().split())
-    if n1<n2:
-        print("#%s"%tc, '<')
-    elif n1>n2:
-        print("#%s"%tc, '>')
-    else:
-        print("#%s"%tc, '=')
